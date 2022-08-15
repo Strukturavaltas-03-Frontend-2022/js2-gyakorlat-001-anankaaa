956f3f0eac7165949ab294ae85a7fe43187fe1a4
Anankaaa <hegyaljai83@hotmail.com>
2022-08-15 21:56:12 +0200

    js2-gyakorlat-1-kész

--- js/01/objectsMerge.js
+++ js/01/objectsMerge.js
@@ -1,3 +1,19 @@
-const objectsMerge = () => {};
+const johnDoe = { 
+    firstName: 'John',
+    lastName: 'Doe'
+};
+
+const janeDoe = { 
+    firstName: 'Jane',
+    lastName: 'Doe'
+}
+
+const objectsMerge = (...objects) => {
+    let result = {};
+    for(let i=0; i<objects.length; i++) {
+        result[i] = objects[i];
+    }
+    return result;
+};
 
 export default objectsMerge;
